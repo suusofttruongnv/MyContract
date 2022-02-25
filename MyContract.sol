91a3e6a733c512e8ea05674e3af3396c0b483bc0
Nguyen Truong <suusoft.truongnv@gmail.com>
2022-02-25 11:41:04 +0700

    add file

---
+++ MyContract.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract MyContract {
+
+	string public tokenName = "MyContract";
+	string public tokenSymbol = "MCT";
+	uint256 public totalSupply = 100000000000000000;
+	uint8 public decimals = 8;
+
+    constructor() public {}
+
+}
